6516f8869eb17a0bf3eeecd2a562484faef5b23a
Timothy Moose <tim@swiftkickmobile.com>
2025-09-30 11:01:11 -0500

    Make error intent codable

--- Sources/SwiftRepoCore/Error/ErrorIntent.swift
+++ Sources/SwiftRepoCore/Error/ErrorIntent.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// A standard enum indicating an intent or purpose for the error from the perspective of the caller
-public enum ErrorIntent: Sendable {
+public enum ErrorIntent: String, Sendable {
     case dispensable
     case indispensable
 }
