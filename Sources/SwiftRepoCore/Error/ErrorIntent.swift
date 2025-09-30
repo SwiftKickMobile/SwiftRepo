1bd08bff846525ced487761d229aeaf3ed87d03b
Timothy Moose <tim@swiftkickmobile.com>
2025-09-30 11:11:20 -0500

    Fix codable conformance

--- Sources/SwiftRepoCore/Error/ErrorIntent.swift
+++ Sources/SwiftRepoCore/Error/ErrorIntent.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// A standard enum indicating an intent or purpose for the error from the perspective of the caller
-public enum ErrorIntent: String, Sendable {
+public enum ErrorIntent: Codable, Sendable {
     case dispensable
     case indispensable
 }
